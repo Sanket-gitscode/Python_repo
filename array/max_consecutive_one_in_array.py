d22e56b0a87a64ded725790022e3ec79a50fc62e
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-11 03:41:19 +0530

    count of one maximum time in array consecutively

---
+++ array/max_consecutive_one_in_array.py
@@ -0,0 +1,19 @@
+def find_max_consecutive_ones(array: list[int]):
+    
+    count = 0 
+    max_count = 0 
+    
+    for num in array:
+        if num == 1:
+            count += 1 
+            if count > max_count :
+                max_count = count
+            
+        else:
+            count = 0 
+        
+    return max_count
+
+
+nums = [1, 1, 0, 1, 1, 1]
+print(find_max_consecutive_ones(nums))
