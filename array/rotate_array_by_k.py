7a4c3e2de6695840ca0491ad5ccc944f57493045
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-13 04:40:01 +0530

    Added roate array by k using revrsal algotithm

---
+++ array/rotate_array_by_k.py
@@ -0,0 +1,31 @@
+def rotate_arry_by_k(array : list[int], k : int):
+    
+    def array_reverse(array,left,right): # basic array reversal using two pointers technique 
+        
+        while left < right :
+            array[left],array[right] = array[right],array[left]
+            left  += 1
+            right -= 1
+            
+    
+    n = len(array) - 1
+    
+    k =  k %  n   # if k > n for wrapping around the array we use modulo 
+
+    array_reverse(array,0,n)
+    array_reverse(array,0,k)
+    array_reverse(array,k,n)
+    
+    return array
+
+# Test case
+arr = [1,2,3,4,5]
+k = 2
+
+
+g = 0 
+for i in range(1,11):
+    print(f"g for now is {g}")
+    print(rotate_arry_by_k(arr,g))
+    g += 4
+    
