fc0a9d9e54072b4243ee046c7b292b28f88c8f57
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-12 13:24:02 +0530

    added py logic for checking if given strings are anagram of each other

---
+++ array/valid_anagram.py
@@ -0,0 +1,29 @@
+def is_anagaram(string1 : str , string2 : str):
+    
+    if len(string1) != len(string2):
+        return False
+    
+    char_freq = {}
+    
+    for ch in string1:
+        
+        char_freq[ch] = char_freq.get(ch,0) + 1
+    
+    
+    for ch in string2:
+        if ch not in char_freq:
+            return False
+        
+        char_freq[ch] -= 1
+        
+    
+    return True 
+    
+
+
+
+#Test case 01 
+s1 = "silent"
+s2 = "listenw"
+     
+print(is_anagaram(s1,s2))
