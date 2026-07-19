b02c28c4d53156842223a244f50ba1690753477a
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-19 20:24:09 +0530

    Added fix and test case

--- string/logest_common_prefix.py
+++ string/logest_common_prefix.py
@@ -6,9 +6,15 @@ def longest_common_prefix(strings : list):
         for j in strings[1:]:
             if  i >= len(j) or check_word[i] != j[i]:
                 return check_word[:i]
-                      
+    
+    return check_word                 
         
         
 #Test case 01 
 strs = ["flower","flow","flight"]
 print(longest_common_prefix(strs))
+
+#Test case 02 
+
+str2 =['dog','dog','dog']
+print(longest_common_prefix(str2))
