77cd82e125efa4ffcaa091116eb34f97fec5000c
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-19 09:43:13 +0530

    changed logic from open to a defined funtion

--- string/reverse_a_string.py
+++ string/reverse_a_string.py
@@ -1,15 +1,18 @@
+def reverse_a_string(text):
+    
+    char_list = list(text)
+    left = 0
+    right = len(char_list) - 1
+
+    while left < right:
+        char_list[left], char_list[right] = char_list[right], char_list[left]
+        left += 1
+        right -= 1
+
+    reversed_text = "".join(char_list)
+
+    return reversed_text
+    
 text = "Hello World"
-
-char_list = list(text)
-
-left = 0
-right = len(char_list) - 1
-
-while left < right:
-    char_list[left], char_list[right] = char_list[right], char_list[left]
-    left += 1
-    right -= 1
-
-reversed_text = "".join(char_list)
-
-print(reversed_text)
+print(reverse_a_string(text))
+    
