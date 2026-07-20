e7322d0ead59a65bef259b60465e4b72dc469b14
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-20 09:46:08 +0530

    added edge case to lcp

--- string/logest_common_prefix.py
+++ string/logest_common_prefix.py
@@ -1,15 +1,17 @@
-def longest_common_prefix(strings : list):
-    
-    if len(strings) < 1 :
-        return False
-    
-    check_word = strings[0]
-    
+def longest_common_prefix(strings: list[str]) -> str:
+
+    if not strings:
+        return ""
+
+    check_word = min(strings, key=len)
+
     for i in range(len(check_word)):
-        for j in strings[1:]:
-            if i >= len(j) or check_word[i] != j[i]:
+
+        for word in strings:
+
+            if check_word[i] != word[i]:
                 return check_word[:i]
-    
+
     return check_word
         
 #Test case 01 
