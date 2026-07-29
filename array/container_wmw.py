d15f2bd3223f988488e05a493ddf4ce69819bf72
Sanket-gitscode <sanketyt11@gmail.com>
2026-07-29 20:49:33 +0530

    added an optimal solution

--- array/container_wmw.py
+++ array/container_wmw.py
@@ -13,6 +13,27 @@ def maxArea_bruteforce(height):
 
         return max_water
 
-height = [1,8,6,2,5,4,8,3,7]
 
+def maxArea_two_optimal(height):
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+
+    while left < right:
+        width = right - left
+        h = min(height[left], height[right])
+        area = width * h
+
+        max_water = max(max_water, area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_water
+
+
+height = [1,8,6,2,5,4,8,3,7]
 print(maxArea_bruteforce(height))
+print(maxArea_two_optimal(height))
