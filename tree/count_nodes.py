aea010b57681e5f108cea04c78091ab42b6a9473
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-07 19:40:05 +0530

    added search insert postion leet35

--- tree/count_nodes.py
+++ tree/count_nodes.py
@@ -12,8 +12,8 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
-    
-root = Node(1)
+        
+root = Node(1)  
 root.left = Node(2)
 root.right = Node(3)
 
