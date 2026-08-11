c988ea96afc6e394b0cef6641dcf7b7ad275a115
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-11 06:32:30 +0530

    added height of tree solution

---
+++ tree/height_of_tree.py
@@ -0,0 +1,34 @@
+'''
+    1
+   / \
+  2   3
+ / \
+4   5 
+
+'''
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+root = Node(1)  
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.left.left.left  = Node(6)
+
+def height_of_tree(root):
+    
+    if root is None:
+        return -1 
+    
+    left_subtree_height = height_of_tree(root.left)
+    right_subtree_height = height_of_tree(root.right)
+    
+    return max(left_subtree_height,right_subtree_height) + 1
+
+
+print(height_of_tree(root))
