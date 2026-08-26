4fde809aac47b0488c734b3860a5ef7876ff63c0
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-26 22:16:07 +0530

    added plus One Contempary solution

--- other_problems/plus_one.py
+++ other_problems/plus_one.py
@@ -5,6 +5,18 @@ def plusOne(digits: list[int]) -> list[int]:
    
    return  list(map(int,str(num)))
     
+def plusOne_othersolution(digits: list[int]) -> list[int]:
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+
+        digits[i] = 0
+
+    return [1] + digits
+
+
 
 given_digit = [1,2,3]
 print(plusOne(given_digit))
+print(plusOne_othersolution(given_digit))
