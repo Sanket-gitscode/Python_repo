c26595cf30b4d7aeb3d0c51949cfe6df0c840ca7
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-27 15:08:31 +0530

    corrected the previous solution which adds one regarless of array

--- other_problems/plus_one.py
+++ other_problems/plus_one.py
@@ -4,19 +4,22 @@ def plusOne(digits: list[int]) -> list[int]:
    num += 1
    
    return  list(map(int,str(num)))
+
+
+def plusone_othersolution(array):
     
-def plusOne_othersolution(digits: list[int]) -> list[int]:
-    for i in range(len(digits) - 1, -1, -1):
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
+    for i in range(len(array) - 1, -1, -1):
+        if array[i] < 9:
+            array[i] += 1
+            break
+        else:
+            array[i] = 0
+    else:
+        array.insert(0, 1)
 
-        digits[i] = 0
-
-    return [1] + digits
-
+    return array
 
 
 given_digit = [1,2,3]
 print(plusOne(given_digit))
-print(plusOne_othersolution(given_digit))
+print(plusone_othersolution(given_digit))
