dfa4fae8948c3d31f0cef1c6acf195562a735426
Sanket-gitscode <sanketyt11@gmail.com>
2026-09-01 22:05:50 +0530

    added a better solution

--- other_problems/pascal_triangle.py
+++ other_problems/pascal_triangle.py
@@ -44,3 +44,16 @@ def pascal_traingle2( rowIndex: int):     # bad solution but works as we build t
                 
         return triangle[rowIndex]
     
+    
+def pascal_traingle2_better(rowIndex: int):
+    row = [1]
+
+    for i in range(1,rowIndex+1):
+        row.append(1)
+
+        for j in range(i-1,0,-1):
+            row[j] = row[j-1] + row[j]
+
+    return row
+
+print(pascal_traingle2_better(3))
